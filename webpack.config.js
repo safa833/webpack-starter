6bb52356da2d686a0b88accca9152dd4c0a9d1a8
BSF <bsf@Akashs-Macbook-Air.local>
2024-02-12 17:49:46 +0530

    loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlwebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode : 'development',
@@ -7,11 +8,46 @@ module.exports = {
     },
     output :{
          path : path.resolve(__dirname , 'dist'),
-         filename : 'bundle.js'
+         filename : '[name][contenthash].js',
+         clean : true,
+         assetModuleFilename:'[name][ext]'
         },
+    devServer : {
+         static : {
+            directory : path.resolve(__dirname ,'dist')
+         },
+         port : 3000,
+         open : true,
+         hot : true,
+         compress: true,
+         historyApiFallback : true
+    },
     module : {
         rules : [{
-            
-        }]
-    }
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                        }
+                }
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+        },
+        ]
+    },
+    plugins : [
+        new HtmlwebpackPlugin({
+            title : 'Webpack App',
+            filename : 'index.html',
+            template : 'src/template.html'
+        })
+    ]
 }
